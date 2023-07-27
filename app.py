49290acbfae99b56a91c71b2b706e10b75b931bc
dusanBirta <31896340+dusanBirta@users.noreply.github.com>
2023-07-26 21:36:18 -0400

    Updated Files

--- app.py
+++ app.py
@@ -3,75 +3,72 @@ import streamlit as st
 import base64
 from PIL import Image
 import io
-
-def predict_with_yolov8(img_bytes):
-    # Load the YOLOv8 model
-    model = YOLO('best.pt')
-
-    # Convert the image bytes to PIL image
-    pil_image = Image.open(io.BytesIO(img_bytes))
-
-    # Save the PIL image temporarily for YOLOv8 prediction
-    pil_image.save("temp_image.jpg")
-
-    # Run YOLOv8 segmentation on the image
-    results = model('temp_image.jpg', save=True, project="../Results/")
-
-    # Check if the result contains any images
-    if len(results.imgs) > 0:
-        # Access the first image's detection result
-        detection_result = results.imgs[0]
-        seg_result = Image.open(detection_result)
-        return seg_result
-    else:
-        st.error("Error: YOLOv8 segmentation failed. Please try another image.")
-        return None
+import cv2
+import torch
+import numpy as np
 
 def main():
-    st.title("YOLOv8 Predictions with Mouse Click App")
+    st.title("Snapdetect")
 
     # File uploader to get the image from the user
     uploaded_file = st.file_uploader("Upload an image", type=["jpg", "jpeg", "png"])
 
     if uploaded_file is not None:
         # Read the image as bytes
-        img_bytes = uploaded_file.read()
+        image = Image.open(uploaded_file)
+        img_array = np.array(image)
 
-        # Display the uploaded image
-        st.image(img_bytes, use_column_width=True, caption="Uploaded Image")
+        # Get image dimensions and calculate display dimensions maintaining aspect ratio
+        height, width = img_array.shape[:2]
+        display_width = 780  # Reduced a bit to prevent cut-offs
+        display_height = int((display_width / width) * height)
 
-        # Custom HTML template for the click event
+        # Load custom YOLO model named "best.pt"
+        model = YOLO('best.pt')
+
+        # Make predictions using the uploaded image
+        results = model(img_array)
+        result = results[0]  # Extract the first result
+
+        # Process the result and draw bounding boxes and labels
+        boxes = result.boxes
+        for (x1, y1, x2, y2, conf, class_num) in boxes.data:
+            label = result.names[int(class_num)]
+            color = [int(c) for c in COLORS[int(class_num) % len(COLORS)]]
+            cv2.rectangle(img_array, (int(x1), int(y1)), (int(x2), int(y2)), color, 3)
+            cv2.putText(img_array, label, (int(x1), int(y1) - 10), cv2.FONT_HERSHEY_SIMPLEX, 1, color, 3)  # Increased font size
+
+        # Convert the image array to bytes
+        is_success, img_buffer = cv2.imencode(".png", img_array)
+        img_bytes = img_buffer.tobytes() if is_success else None
+
+        # Custom HTML template for click events
         click_html = f"""
         <div style="position: relative; display: inline-block;">
-            <img src="data:image/png;base64,{base64.b64encode(img_bytes).decode()}" alt="Image" width="400" height="400"
-                 onclick="handleClick(event)" onmousemove="handleMouseOver(event)">
+            <img src="data:image/png;base64,{base64.b64encode(img_bytes).decode()}" alt="Image" width="{display_width}" height="{display_height}" onclick="handleClick(event)" onmousemove="handleMouseOver(event)">
         </div>
         <script>
-            // Dictionary to store descriptions and coordinates
             var descriptions = {{}};
-
             function handleClick(event) {{
-                var mouseX = event.clientX;
-                var mouseY = event.clientY;
-                var description = prompt("Enter a description for this area:");
+                var rect = event.target.getBoundingClientRect();
+                var mouseX = event.clientX - rect.left;
+                var mouseY = event.clientY - rect.top;
+                var description = prompt("Enter a price for this item:");
                 if (description) {{
                     descriptions[mouseX + '-' + mouseY] = description;
                 }}
             }}
-
             function handleMouseOver(event) {{
-                var mouseX = event.clientX;
-                var mouseY = event.clientY;
-
+                var rect = event.target.getBoundingClientRect();
+                var mouseX = event.clientX - rect.left;
+                var mouseY = event.clientY - rect.top;
                 var messageDiv = document.getElementById('message');
                 messageDiv.innerHTML = '';
-
-                // Display descriptions for the hovered area
                 for (var key in descriptions) {{
                     var coords = key.split('-');
-                    var distance = Math.sqrt((mouseX - coords[0]) ** 2 + (mouseY - coords[1]) ** 2);
-                    if (distance <= 50) {{  // Only display descriptions within a radius of 50 pixels
-                        messageDiv.innerHTML += '<p style="position: absolute; left: ' + coords[0] + 'px; top: ' + coords[1] + 'px; background-color: #555; color: #fff; border-radius: 6px; padding: 5px;">' + descriptions[key] + '</p>';
+                    var distance = Math.sqrt((mouseX - parseFloat(coords[0])) ** 2 + (mouseY - parseFloat(coords[1])) ** 2);
+                    if (distance <= 50) {{
+                        messageDiv.innerHTML += '<p style="position: absolute; left: ' + (rect.left + parseFloat(coords[0])) + 'px; top: ' + (rect.top + parseFloat(coords[1])) + 'px; background-color: #555; color: #fff; border-radius: 6px; padding: 5px;">' + descriptions[key] + '</p>';
                     }}
                 }}
             }}
@@ -79,15 +76,10 @@ def main():
         <div id="message" style="position: absolute; top: 0; left: 0;"></div>
         """
 
-        # Display the custom HTML template for the click event
-        st.components.v1.html(click_html, height=400, scrolling=False)
+        st.components.v1.html(click_html, height=display_height, scrolling=False)
 
-        # Predict with YOLOv8
-        seg_result = predict_with_yolov8(img_bytes)
-
-        # Display YOLOv8 predictions
-        if seg_result is not None:
-            st.image(seg_result, use_column_width=True, caption="YOLOv8 Predictions")
+# Muted colors for bounding boxes
+COLORS = [(100, 100, 100), (150, 75, 0), (75, 0, 130), (0, 75, 75), (0, 100, 0), (100, 0, 0), (50, 50, 50), (0, 50, 50), (50, 0, 50)]
 
 if __name__ == "__main__":
     main()
